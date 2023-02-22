c586ad8a24111269d1cccf1cbff844ce6d233d6f
hidden-machina <hei.franco.3@gmail.com>
2023-02-22 11:32:14 +0300

    chore: my branche

---
+++ cypress/e2e/enter.data.into.TODO.cy.js
@@ -0,0 +1,8 @@
+import App from "../../src/App";
+describe('test for TODO', () => {
+  it('should enter a new value into TODO', () => {
+    const toTest = cy.mount(<App/ >);
+    cy.contains("")
+
+  })
+})
