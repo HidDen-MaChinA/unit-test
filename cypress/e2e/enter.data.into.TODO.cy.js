640bb190604960639b33a1d76ed4ebcade7d0a2a
hidden-machina <hei.franco.3@gmail.com>
2023-03-01 09:25:05 +0300

    chore:added test

--- cypress/e2e/enter.data.into.TODO.cy.js
+++ cypress/e2e/enter.data.into.TODO.cy.js
@@ -1,8 +1,20 @@
-import App from "../../src/App";
-describe('test for TODO', () => {
-  it('should enter a new value into TODO', () => {
-    const toTest = cy.mount(<App/ >);
-    cy.contains("")
+import { mount } from 'cypress/react18'
+import App from '../../src/App'
 
-  })
-})
+describe("for  App component e2e",()=>{
+        it("should enter a text hello on enter",()=>{
+            cy.visit("http://localhost:3000/")
+                .get("#add-todo")
+                .type("hello{enter}");
+            cy.get("#hello-1").should("contain.text","hello")     
+        })
+         it("should pass value into todo when you click on the checkbox",()=>{
+            cy.visit("http://localhost:3000/")
+                .get("#add-todo")
+                .type("another test{enter}");
+            
+            cy.get("#another test-1").click();
+            cy.get()
+        })
+    }
+)
